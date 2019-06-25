22325fd5f77a37fc4d4848369cb5920f6512bbc2
chenshengnan <962372515@qq.com>
2019-06-25 15:44:36 +0800

    新增测试

---
+++ ocService/src/test/java/com/online/college/service/DemoTest.java
@@ -0,0 +1,24 @@
+package com.online.college.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * ClassName :DemoTest
+ * Description:
+ * Author: 陈胜楠
+ * Date: 2019/6/25 3:42 PM
+ * Version 1.0
+ */
+public class DemoTest {
+        @Test
+        public void testReturnString(){
+            Assert.assertEquals("test","test");
+        }
+        @Test
+        public void testReString(){
+            Assert.assertEquals("test","test");
+        }
+
+
+}
